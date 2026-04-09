25d22a68eee94db30be904b76966662f922b0bc9
Paulo garbo <paulo.garbo@unochapeco.edu.br>
2026-04-09 17:03:55 -0300

    envio com dados de logica atualizado

--- src/controllers/controllerUsuarios.js
+++ src/controllers/controllerUsuarios.js
@@ -22,61 +22,132 @@ const get = async (req, res ) => {
     }
 }
 
-
+//declaraao de uma funcao chamada getlancamentos
+// async e uma funcao asincrona ela roda fora de tempo
+//uma funcao assincrona faz o sistema entender que pode demorar a retornar o valor e nao trava o sistema
 const getlancamentos = async (req, res) => {
+    //req e res sao os retornos req e e o valor requisitado
+    //res e ovalor de rotorno da api express
+    
+    //try e um sistema de erro caso o sistema de algum erro ele tem onde cair
     try {
+        //aqui o const {id} pega o valor do index da rota ex getlancemetos/1
+        //ele vai salvar os dados da requisicao em id
+        //como os dados do index vem com string todas as vezes
         const { id } = req.params;
+        //nessa parte vamos salvar idususarionumero e integrar a ele o valor de number(id)
+        //neste caso o valor de string de id vai ser conventido em numeros e salvo idusuarionumero
         const idUsuarioNumero = Number(id);
-
+        //aqui temos um if de validacao para ver se o numero do index passado par idusuarionumero
+        //e um numero inteiro e se e mais que zero 
+        //a logica vai ficar se numero na negacao chamada de isINTEGER que verifica se for interiro ou nao 
+        //e retonar um true ou false ou seja se numero negado for inteiro ele vai negar se nao for intero ele vai ganhar true
+        //se ele ganhar true ele vai chamar o erro 400
         if (!Number.isInteger(idUsuarioNumero) || idUsuarioNumero <= 0) {
+
+            //se o numero nao for inteiro ele vai receber verdadeiro e cair aqui entao vai retornar o erro
+            //no casso res e a resposta da api e status 
+            //nesta linha o status e uma forma de devolver um estado ou comunicacao ja cadastrada para os usuarios
+            //no casso o status vai ser o 400 e o send envia uma resposta do servido para quem fez a requisicao
             return res.status(400).send({
                 type: 'error',
                 message: 'id invalido',
                 data: []
             });
         }
-
+        // aqui temos uma definicao de uma variacel que e a usuarios com await no caso 
+        //quando usamos await o js vai esperar a resquisicao do servido para passar para o proximo passo
+        //seria como dizer ao js espere ate eu devolver algo 
+        //aqui ele esta esperando algo da tabela dadosususario 
+        //a logica vai fincar constante :  usuarios espere a busca na tabela dados usuarios
+        // a busca sera feita pela primarykey e estÁ buscando a igual idusuarioNumero 
         const usuario = await DadosUsuarios.findByPk(idUsuarioNumero);
-
+        //aqui caso nao tenha dados retornados a usuario ele recebera null
+        //entao se o valor e null ele e falso
+        //aqui o if vai verificar se usuarios for null ele retorna este erro
         if (!usuario) {
+            //aqui o memso sentido o res ou seja retorno da api ele vai reornar o erro 404
             return res.status(404).send({
                 type: 'error',
                 message: 'usuario nao encontrado',
                 data: []
             });
         }
+        //aqui ele vai esperar uma busca total no historico e esperar o findall
+        //serve para realizar uma busca completa
 
         const dados = await Historico.findAll({
+            //aqui fizemos uma bus mais especifica dentro do finall dizemos quais tabelas e campos queremos trazer
+            //no casso passado este atributos eu etou dizendo que quero traer o id_sinopse
             attributes: [
                 'id_sinopse',
+                //o sequelize.fn e uma funcao usada para poder fazer requisicoes sql dentro do 
+                //proprio sequelize no nosso caso o count que vai fazer uma conta
+                //emos o sequelize col que serve para fazer uma busca em colunas e nao tabelas
+                //ou seja passamos que temos a nossa pk id_sinopse e vamos fazer uma contagem 
+                //essa contagem vai comecar na tabela historico 
+                //nesse caso ele vai contar nesta tabela seguindo esta logica
+                //agrupando o id sinopse passa o campo a arupar o cont vai contar porem 
+                //o sequelize col passa a coluna a ser contada e total assistido vai ser a saido
+                //no caso vai guardar o resultado
+                
                 [sequelize.fn('COUNT', sequelize.col('historico.id_sinopse')), 'total_assistido']
             ],
+
+
+            //aqui este include 
+            //ele vai ajudar a puxar nao so a tabela historico mais dados relacionados a ela
+            //com ligacao
+            //igual a um join
             include: [
                 {
+                    //aqui o model diz em qual tabela vamos conectar 
                     model: PerfisUsuarios,
+                    //aqui sao os parametros dos dados no caso vamos puxar em perfisUsuarios
+                    //vamos fazer uma comparacao onde o id de perfis usuarios for igual ao index
                     as: 'perfil_usuario',
                     attributes: [],
                     where: { id_dados_usuario: idUsuarioNumero }
                 },
+                //aqui o sentido e o mesmo porem vamos puxar os atributos passado sem passar comparacao 
                 {
                     model: Sinopse,
                     as: 'Sinopse',
                     attributes: ['id', 'data_lancamento', 'informacoes']
                 }
+
+
             ],
+            //aqui o comando group serve para agrupar os dados puxados de ...
             group: ['historico.id_sinopse', 'Sinopse.id'],
+            //ja o order serve para ordenar seguindo tal parametros
+            //no nosso caso vamos ultilizar o sequelize literal que serve para 
+            //podermos escrever comandos sql dentro do codigo 
+            //aqui vamos  chamar total assitido que e como se fosse uma variavel temporaria
+            //ja o desc e um sentido de ordenacao do caso ele representa o decresente
             order: [
                 [sequelize.literal('total_assistido'), 'DESC'],
+                //no casso aqui temo uma estrututacao de capo e diecao 
+                // [campo [direcao]] 
+                //este model serve para idicar de qual tabela vem o campo para ordenar
+                //o as serve para indicar o nome da acossicao o data lancamento e o campo e o desc serve para 
+                //ordenacao no caso o desc serve para para dizer o metodo de ordenacao c
                 [{ model: Sinopse, as: 'Sinopse' }, 'data_lancamento', 'DESC']
             ],
+
+            //esse limit serve para que o retono mostre apenas um conjunto de dados 
+            
             limit: 1
         });
-
+        //aqui temos um alert de sucesso na busca
         return res.status(200).send({
             type: 'sucess',
             message: 'lancamento mais assistido encontrado com sucesso',
             data: dados
         });
+
+        //caso o try la em cima de errado ele vai cair aqui 
+        //no casso se der erro ele cai neste erro 
     } catch (error) {
         return res.status(500).send({
             type: 'error',
